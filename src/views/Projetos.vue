1cd46be6157bdb6c023301f7cccb460da49f229e
Iago Sousa <73495537+iagosm@users.noreply.github.com>
2024-08-07 20:53:30 -0300

    Manipulação de estado, rotas aninhadas, tipos de mutações

--- src/views/Projetos.vue
+++ src/views/Projetos.vue
@@ -1,60 +1,15 @@
 <template>
-  <section class="projetos">
+  <div class="projetos">
     <h1 class="title">Projetos</h1>
-    <form @submit.prevent="salvar">
-      <div class="field">
-        <label for="nomeDoProjeto" class="label">
-          Nome do Projeto
-        </label>
-        <input type="text" class="input" v-model="nomeDoProjeto" id="nomeDoProjeto">
-      </div>
-      <div class="field">
-        <button type="submit" class="button">
-          Salvar
-        </button>
-      </div>
-    </form>
-    <table class="table is-fullwidth">
-      <thead>
-        <tr>
-          <td>ID</td>
-          <td>Nome</td>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="projeto in projetos" :key="projeto.id">
-          <td>{{ projeto.id }}</td>
-          <td>{{ projeto.nome }}</td>
-        </tr>
-      </tbody>
-    </table>
-  </section>
+    <router-view></router-view>
+  </div>
 </template>
-<script lang="ts">
-import { computed, defineComponent } from 'vue';
-import { useStore } from '@/store';
 
+<script lang="ts">
+import { defineComponent } from 'vue';
 export default defineComponent({
-  name:'ProjetosComp',
-  data() {
-    return {
-      nomeDoProjeto: '',
-    }
-  },
-  methods: {
-    salvar() {
-      this.store.commit('ADICIONA_PROJETO', this.nomeDoProjeto)
-      this.nomeDoProjeto = '';
-    }
-  },
-  setup () {
-    const store = useStore();
-    return {
-      store,
-      projetos: computed(() => store.state.projetos)
-    }
-  }
-})
+  name: 'ProjetosAll'
+}); 
 </script>
 <style scoped>
 .projetos {
